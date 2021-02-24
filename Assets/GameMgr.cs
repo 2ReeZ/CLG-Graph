8417a1fb8f4c65fe4e58ec8d5c939de662e4d0a2
Arturo Dominguez <arturo.dominguez42@gmail.com>
2021-02-24 10:08:32 -0600

    commit on main

---
+++ Assets/GameMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    // create a reference to the game manager.
+    public static GameMgr manager;
+
+    // awake is called before Start().
+
+    void Awake()
+    {
+       if(manager != null) 
+       {
+           Destroy(this.gameObject);
+           Debug.Log("There is another Skywalker.");
+       } else
+       {
+           manager = this;
+           Debug.Log("There can only be one!");
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
